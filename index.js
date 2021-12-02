43dbb411f460710b496316c12dc185620e639dd7
Gary Flugel <flugelg@gmail.com>
2021-12-02 02:42:24 -0500

    commit tree 2369743b018e7af52dc1511ab5570709b44ef7b9

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solution in this file!
+const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia'];
+
+const returnFirstTwoDrivers = function(){
+    return drivers.slice(0,2);
+}
+const returnLastTwoDrivers = function(){
+    return drivers.slice(2,4);
+}
+
+let selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers];
+
+const createFareMultiplier = function(tickets) {
+    return function(fare){
+        return  tickets * fare;
+    }
+}
+
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = function(drivers, cb){
+    return cb(drivers);
+}
